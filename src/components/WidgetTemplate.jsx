e0889caa3764e95f6d94ccc3f78be2768abeb9d9
Aj Blosser <ablosser@wvuf.org>
2017-02-02 08:47:50 -0500

    added proptypes

--- src/components/WidgetTemplate.jsx
+++ src/components/WidgetTemplate.jsx
@@ -1,20 +1,41 @@
 import React, { Component, PropTypes  } from 'react';
 import CSSModules from 'react-css-modules';
 
-
 import Sass from '../sass/style.scss';
-//import Bulma from './wt.css';
-
+import proptypes from '../lib/proptypes';
 
 class WidgetTemplate extends Component {
 
-  render() {
+  //###################### PROPERTIES
+  state = {}
 
+  static propTypes = {
+    title: proptypes.optionalString;
+  }
+
+  static defaultProps = {
+    title: 'Your Name'
+  }
+
+  //###################### METHODS
+    handleSubmit = (e) => {
+      e.preventDefault()
+
+    }
+
+  //###################### RENDER
+
+  render() {
+    const {
+      title
+    } = this.props;
 
     return <div styleName="section">
               <div styleName="container">{this.props.children}</div>
           </div>
   }
+
+
 }
 
 export default CSSModules(WidgetTemplate,Sass,{allowMultiple:true});
