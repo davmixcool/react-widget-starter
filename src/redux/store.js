2c614115d118725e28a65035416fe014057b248c
Aj Blosser <ablosser@wvuf.org>
2017-05-23 10:14:35 -0400

    added redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,7 @@ import promise from 'redux-promise-middleware';
 
 import reducer from './reducers';
 
-const middleware = applyMiddleware(promise(), thunk, logger());
-
-export default createStore(reducer, middleware);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export default createStore(reducer, composeEnhancers(
+    applyMiddleware(promise(), thunk, logger())
+  ));
