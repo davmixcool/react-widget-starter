573374a88d237aeff0d48a0a74e220633a4963b2
Aj Blosser <ablosser@wvuf.org>
2017-05-25 08:47:42 -0400

    added Reactotron

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import {applyMiddleware, createStore, compose} from 'redux';
-
+import Reactotron from 'reactotron-react-js';
 import { createLogger as logger} from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
@@ -7,6 +7,6 @@ import promise from 'redux-promise-middleware';
 import reducer from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export default createStore(reducer, composeEnhancers(
+export default Reactotron.createStore(reducer, composeEnhancers(
     applyMiddleware(promise(), thunk, logger())
   ));
