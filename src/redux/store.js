ac829825eb49bb3d4de1e0a7608bb81b0b57f7e0
Aj Blosser <ablosser@wvuf.org>
2017-06-21 11:24:46 -0400

    Webpack 3

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,24 @@
-import {applyMiddleware, createStore, compose} from 'redux';
-import Reactotron from 'reactotron-react-js';
-import './ReactotronConfig';
-import { createLogger as logger} from 'redux-logger';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise-middleware';
+import { applyMiddleware, createStore, compose } from "redux";
+import Reactotron from "reactotron-react-js";
+import "./ReactotronConfig";
+import { createLogger as logger } from "redux-logger";
+import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
 
-import reducer from './reducers';
+import reducer from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export default Reactotron.createStore(reducer, composeEnhancers(
-    applyMiddleware(promise(), thunk, logger())
-  ));
+
+if (process.env.NODE_ENV !== "production") {
+  var store = Reactotron.createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(promise(), thunk, logger()))
+  );
+} else {
+  var store = createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(promise(), thunk, logger()))
+  );
+}
+
+export default store;
